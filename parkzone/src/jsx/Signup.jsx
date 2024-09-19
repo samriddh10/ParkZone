2ed971223ac57ae0f7692a4bdaceebf1ea94ea25
samriddh10 <202251117@iiitvadodara.ac.in>
2024-09-20 00:41:40 +0530

    Friday20Sep

--- parkzone/src/jsx/Signup.jsx
+++ parkzone/src/jsx/Signup.jsx
@@ -1,115 +1,148 @@
 import Header from "./Header";
 import "./css/Signup.css";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
+import React, { useState } from "react";
 
 function Signup() {
   const navigate = useNavigate(); // Initialize useNavigate
+  const [showDialog, setShowDialog] = useState(true);
+  const [signUpType, setSignUpType] = useState("");
 
   const handleSignupClick = () => {
     navigate("/"); // Navigate to the signup route
+  };
+  const handleUserClick = () => {
+    console.log("User selected");
+    setSignUpType("User");
+    setShowDialog(false); // Close dialog
+  };
+
+  const handleOwnerClick = () => {
+    console.log("Owner selected");
+    setSignUpType("Owner");
+    setShowDialog(false); // Close dialog
   };
 
   return (
     <div>
       <Header />
 
-      <div 
-        className="signup"
-        style={{
-          backgroundImage: "url(/assets/car-parking-app.png)",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        
+      {showDialog && (
+        <div className="dialog">
+          <h2>Sign In as:</h2>
+          <button className="dialog-button" onClick={handleUserClick}>
+            User
+          </button>
+          <button className="dialog-button" onClick={handleOwnerClick}>
+            Owner
+          </button>
+        </div>
+      )}
+      <div className={showDialog ? "blurred-background" : ""}>
+        <div
+          className="signup"
+          style={{
+            backgroundImage: "url(/assets/car-parking-app.png)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
         >
-        <form id="login-form">
-          <div className="top">
-            <div className="logo">ParkZone</div>
-            <div>
-              <input
-                className="userid"
-                type="Name"
-                placeholder="Name"
-                id="name"
-              />
-            </div>
-            <div>
-              <input
-                className="userid"
-                type="date"
-                placeholder="Date of Birth"
-                id="dob"
-              />
-            </div>
-            <div>
-              <input
-                className="userid"
-                type="phone"
-                placeholder="Conatact Number"
-                id="phone"
-              />
-            </div>
-            <div className="userid">
-            Gender
-              <input
-                className="gender"
-                type="radio"
-                name="gender"
-                value="male"
-                id="male"
-              />
-              Male
-              <input
-                className="gender"             
-                type="radio"
-                name="gender"
-                value="female"
-                id="female"
-              />
-              Female
-              <input
-                className="gender"
-                type="radio"
-                name="gender"
-                value="female"
-                id="other"
-              />
-              Other
-            </div>
+          <form id="login-form">
+            <div className="top">
 
-            <div>
-              <input
-                className="userid"
-                type="email"
-                placeholder="Email"
-                id="email"
-              />
-            </div>
-            <div>
-              <input
-                className="userid"
-                type="password"
-                placeholder="Password"
-                id="password"
-                required
-              />
-            </div>
-            <div>
-              <button className="login-button" type="submit">
-                Signup
-              </button>
-            </div>
-            
-          </div>
-          <div className="middle">
-            <div className="signup-box">
-              Already have an account?
-              <div className="signup-button" onClick={handleSignupClick}>
-                Login
+              <div className="signup-type">
+                {signUpType === "User"
+                  ? "User SignUp"
+                  : signUpType === "Owner"
+                  ? "Owner SignUp"
+                  : ""}
+              </div>
+              <div className="logo">ParkZone</div>
+              <div>
+                <input
+                  className="userid"
+                  type="Name"
+                  placeholder="Name"
+                  id="name"
+                />
+              </div>
+              <div>
+                <input
+                  className="userid"
+                  type="date"
+                  placeholder="Date of Birth"
+                  id="dob"
+                />
+              </div>
+              <div>
+                <input
+                  className="userid"
+                  type="phone"
+                  placeholder="Conatact Number"
+                  id="phone"
+                />
+              </div>
+              <div className="userid">
+                Gender
+                <input
+                  className="gender"
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  id="male"
+                />
+                Male
+                <input
+                  className="gender"
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  id="female"
+                />
+                Female
+                <input
+                  className="gender"
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  id="other"
+                />
+                Other
+              </div>
+
+              <div>
+                <input
+                  className="userid"
+                  type="email"
+                  placeholder="Email"
+                  id="email"
+                />
+              </div>
+              <div>
+                <input
+                  className="userid"
+                  type="password"
+                  placeholder="Password"
+                  id="password"
+                  required
+                />
+              </div>
+              <div>
+                <button className="login-button" type="submit">
+                  Sign Up
+                </button>
               </div>
             </div>
-          </div>
-        </form>
+            <div className="middle">
+              <div className="signup-box">
+                Already have an account?
+                <div className="signup-button" onClick={handleSignupClick}>
+                  Login
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
