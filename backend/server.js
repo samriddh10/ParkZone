62f254097f3210a93e4ba3d85ede0a2235a35707
samriddh10 <120704397+samriddh10@users.noreply.github.com>
2024-09-26 16:04:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 // Initialize Express
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ||3000;
 
 app.use(cors());
 // Handle preflight requests
